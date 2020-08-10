57c1163b42d6709500c00fdc3d41fdd677b0fa7d
Gregory Monroe II <gregthemusician@gmail.com>
2020-08-10 18:52:47 -0400

    seperated SearchResults and SavedResults

---
+++ client/src/components/SearchResults/SearchResults.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import Button from "../Button/Button";
+import "./SearchResults.css";
+
+class Results extends Component {
+  render() {
+    return (
+      <div class="container border">
+        <div class="row">
+          <h4>Results</h4>
+          <Button>
+            <p>View</p>
+          </Button>
+          <Button>
+            <p>Save</p>
+          </Button>
+        </div>
+        <br />
+        <div class="row"></div>
+            <div class="container border">
+            <div class="row">
+                <div class="col-sm-9 text-center">
+                    <div class="row">
+                        <h5>title</h5>
+                    </div>
+                    <div class="row">
+                        <p>Written by author</p>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-3">
+                        image
+                        </div>
+                        <div class="col-sm-9">
+                        <p>description</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+      </div>
+    );
+  }
+}
+
+export default Results;
