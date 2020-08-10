b4c3a20e14b0bb68a3ed6534acf60a92a6e5090d
Gregory Monroe II <gregthemusician@gmail.com>
2020-08-10 17:19:48 -0400

    added Search page

--- client/src/containers/Search/Search.jsx
+++ client/src/containers/Search/Search.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navbar from "../../components/Navbar/Navbar";
 import Jumbotron from "../../components/Jumbotron/Jumbotron";
 import BookSearch from "../../components/BookSearch/BookSearch"
 import Results from "../../components/Results/Results"
@@ -7,6 +8,7 @@ import Footer from "../../components/Footer/Footer"
 const Search = () => {
     return (
         <div>
+            <Navbar />
             <Jumbotron />
             <BookSearch />
             <Results />
