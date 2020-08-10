dc6ae8add89d2d45d42abe87ed6113183b5a5d0f
Gregory Monroe II <gregthemusician@gmail.com>
2020-08-10 17:04:11 -0400

    added footer

--- client/src/containers/Search/Search.jsx
+++ client/src/containers/Search/Search.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Jumbotron from "../../components/Jumbotron/Jumbotron";
 import BookSearch from "../../components/BookSearch/BookSearch"
 import Results from "../../components/Results/Results"
+import Footer from "../../components/Footer/Footer"
 
 const Search = () => {
     return (
@@ -9,6 +10,7 @@ const Search = () => {
             <Jumbotron />
             <BookSearch />
             <Results />
+            <Footer />
         </div>
     );
 };
